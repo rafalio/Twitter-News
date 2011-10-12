27b808b495f5d1c738204a0d8e5b3c25d00514dc
Sam Wong <info@samwong.hk>
2011-10-12 17:46:09 +0100

    blah

--- csvparser.py
+++ csvparser.py
@@ -4,25 +4,61 @@ with open('blah.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
         graph.append(row)
-json_graph = []
+
+master = []
+dataset = {}
+adjacencies = []
 for row in graph:
-	json_graph_row = {}
-	json_graph_row['nodeFrom'] = row[0]
-	json_graph_row['nodeTo'] = row[1]
-	json_graph_row['data'] = {}
-	json_graph.append(json_graph_row)
-# print json_graph
- 	
-out_graph = json.dumps(json_graph, indent=4)
-header = 'var json = [ { "adjacencies": '
-footer = '} ]'
-#File operations
-filename = "graph.dat"
+	adjancency_row = {}
+	adjancency_row['nodeFrom'] = row[0]
+	adjancency_row['nodeTo'] = row[1]
+	adjancency_row['data'] = {}
+	adjacencies.append(adjancency_row)
+dataset['adjacencies'] = adjacencies
+dataset['data'] = {
+			          "$color": "#C74243",
+			          "$type": "star",
+			          "$dim": 8
+			        }
+dataset['id'] = "kurwa"
+dataset['name'] = "kurwaSzymanski"
+master.append(dataset);
 
+# Save json as string
+filename = "setJsonAs"
 FILE = open(filename, "w")
-FILE.writelines(header)
-FILE.writelines(out_graph)
-FILE.writelines(footer)
+FILE.writelines(json.dumps(master, indent=4))
 
-# print json_graph
-# filename = "json_graph.txt"
+
+# print jsonArray
+ 	
+
+
+#		 {
+#         "adjacencies": [],
+#         "data": {
+#           "$color": "#C74243",
+#           "$type": "star",
+#           "$dim": 8
+#         },
+#         "id": "graphnode20",
+#         "name": "graphnode20"
+#       }
+
+# 
+# 
+# 
+# 
+# out_graph = json.dumps(jsonArray, indent=4)
+# header = 'var json = [ { "adjacencies": '
+# footer = '} ]'
+# #File operations
+# filename = "graph.dat"
+# 
+# FILE = open(filename, "w")
+# FILE.writelines(header)
+# FILE.writelines(out_graph)
+# FILE.writelines(footer)
+
+# print graph
+# filename = "graph.txt"
