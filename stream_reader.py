b8346a6a69c62dbc429dc58e9749b66fdbd1d4c6
Maciek Albin <madmck@gmail.com>
2011-11-03 15:22:12 +0000

    Date in tweets is now saved as a date, not String

--- stream_reader.py
+++ stream_reader.py
@@ -2,6 +2,7 @@ import pycurl, json, pymongo, ConfigParser, bson, hashlib, threading, itertools
 
 import mongo_connector
 import shared
+from dateutil import parser
 
 class StreamReader(threading.Thread):
 
@@ -27,49 +28,47 @@ class StreamReader(threading.Thread):
       self.getTweetsBySubject(list(itertools.chain.from_iterable(map(lambda a : a["keywords"], shared.stories))),
                               self.receive_and_write_to_Mongo)
       shared.flag = False;
-
+  
   def getTweetsBySubject(self, subjects, onwrite):
-
     print "Starting the stream getting...."
-
+    
     stream_url  = "https://stream.twitter.com/1/statuses/filter.json"
     print subjects
     post_data = "track=" + ",".join(subjects)
     print "post_data = " + post_data
-
+    
     conn = self.openStream(stream_url, onwrite)
     conn.setopt(pycurl.POST, 1)
     conn.setopt(pycurl.POSTFIELDS, post_data)
-
+    
     # Stream until I need to change the keyword list
     try:
       conn.perform()
     except Exception:
       print "Done..."
       conn.close()
-
+  
   def openStream(self, stream, write_function):
     conn = pycurl.Curl()
     conn.setopt(pycurl.USERPWD, "%s:%s" % (self.twitter_username, self.twitter_password))
     conn.setopt(pycurl.URL, stream)
     conn.setopt(pycurl.WRITEFUNCTION, write_function)
     return conn
-
+  
   def on_receive(self, data):
     print json.loads(data)
-
+  
   def receive_and_write_to_Mongo(self, data):
     try:
-
       # Means we need to restart with new set of keywords
       if shared.flag:
-        print "return -1 ASDFASDFASDFASDF"
         # This will get out of the pycurl thread, it will cause
         # an exception
         return -1
 
       data = json.loads(data)
       data["_id"] = bson.objectid.ObjectId(hashlib.md5(str(data["id"])).hexdigest()[:24])
+      data["created_at"] =  parser.parse(data["created_at"])
       print data["text"]
       self.tweet_collection.insert(data)
 
