5f5c35d9e49eb3f47e65192653793951bacf01e7
Maciek Albin <madmck@gmail.com>
2011-11-04 18:01:22 +0000

    Changed keyword limit to be a default call thing in the extractor

--- keyword_extractor.py
+++ keyword_extractor.py
@@ -2,31 +2,33 @@ import ConfigParser, json, urllib
 
 class KeywordExtractor:
   
-  def __init__(self):
+  def __init__(self, limit=3):
     config = ConfigParser.RawConfigParser()
     config.read('config.conf')
     self.key = config.get('alchemyapi','key')
     self.stream = "http://access.alchemyapi.com/calls/url/URLGetRankedKeywords"
     self.apikey_option = 'apikey=' + self.key
     self.output_option = 'outputMode=' + 'json'
-    self.keyword_limit_option = 'maxRetrieve=' + '5'
+    self.keyword_limit_option = 'maxRetrieve={0}'.format(limit)
 
   def getKeywordsByURL(self, url):
     # Building Alchemy API call
     call = self.stream +  '?' + self.apikey_option + "&" + 'url=' + url + "&" + self.output_option + '&' + self.keyword_limit_option
     data = urllib.urlopen(call)
-    keywords = []
     try:
+      keywords = []
+      #Fetching data from Alchemy API
       data = json.loads(data.read())
       keyword_data =  data['keywords']
+      
+      #We only care about the keywords
       for keyword_entry in keyword_data:
         keywords.append(str(keyword_entry['text']))
+      return keywords
     except ValueError:
       print "Error in data from the keyword extractor"
       print data
-    return keywords
+      return []
 
 if __name__ == "__main__":
   k = KeywordExtractor()
-  print k.getKeywordsByURL("http://bit.ly/oTPnyE")
-  print k.getKeywordsByURL("http://techcrunch.com/2011/10/19/dropbox-minimal-viable-product/")
